8bd82b0ddf325cb3888caedaf168bf3357f4df26
saemyunglim <saemyung2000@naver.com>
2020-04-27 23:36:09 +0900

    spring data jpa의 쿼리 메서드 기능 - Spring Data Jpa를 이용한 slice 및 테스트 통과

--- src/test/java/com/lim/datajpa/springboot/repository/MemberRepositoryTest.java
+++ src/test/java/com/lim/datajpa/springboot/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -176,8 +177,40 @@ class MemberRepositoryTest {
         Optional<Member> result3=memberRepository.findOptionalByUsername("aaa");
         System.out.println("result3 = " + result3.get());
     }
+//    @Test
+//    public void paging(){
+//        //given
+//        memberRepository.save(new Member("member1", 10));
+//        memberRepository.save(new Member("member2", 10));
+//        memberRepository.save(new Member("member3", 10));
+//        memberRepository.save(new Member("member4", 10));
+//        memberRepository.save(new Member("member5", 10));
+//
+//        int age=10;
+//        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+//
+//        //when
+//        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+//
+//        //then
+//        List<Member> content = page.getContent();
+//        long totalElements = page.getTotalElements();
+//
+//        for (Member member : content) {
+//            System.out.println("member = " + member);
+//        }
+//        System.out.println("totalElements = " + totalElements);
+//
+//        assertThat(content.size()).isEqualTo(3);
+//        assertThat(totalElements).isEqualTo(5);
+//        assertThat(page.getNumber()).isEqualTo(0);
+//        assertThat(page.getTotalPages()).isEqualTo(2);
+//        assertThat(page.isFirst()).isTrue();
+//        assertThat(page.hasNext()).isTrue();
+//    }
+
     @Test
-    public void paging(){
+    public void slicing(){
         //given
         memberRepository.save(new Member("member1", 10));
         memberRepository.save(new Member("member2", 10));
@@ -189,21 +222,17 @@ class MemberRepositoryTest {
         PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
 
         //when
-        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        Slice<Member> page = memberRepository.findByAge(age, pageRequest);
 
         //then
         List<Member> content = page.getContent();
-        long totalElements = page.getTotalElements();
 
         for (Member member : content) {
             System.out.println("member = " + member);
         }
-        System.out.println("totalElements = " + totalElements);
 
         assertThat(content.size()).isEqualTo(3);
-        assertThat(totalElements).isEqualTo(5);
         assertThat(page.getNumber()).isEqualTo(0);
-        assertThat(page.getTotalPages()).isEqualTo(2);
         assertThat(page.isFirst()).isTrue();
         assertThat(page.hasNext()).isTrue();
     }
